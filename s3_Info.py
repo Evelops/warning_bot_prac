a778d3977bf067d51e45b2c81340abca537fd58d
eomseung-yeol <co6747@naver.com>
2023-04-10 17:11:32 +0900

    Style: 변경 bucket_size G -> GB

--- s3_Info.py
+++ s3_Info.py
@@ -22,6 +22,6 @@ def s3_info():
     # 버킷 크기 조회 byte로 리턴되기 때문에 Giga Byte 변경
     bucket_size = sum([obj.size for obj in s3.Bucket(bucket_name).objects.all()])/1073741824
 
-    s3_info = {'bucket_name': f'{bucket_name}', 'folder_count': f'{folder_count}', 'file_count': f'{file_count}', 'bucket_size': f'{bucket_size:.2f}G'}
+    s3_info = {'bucket_name': f'{bucket_name}', 'folder_count': f'{folder_count}', 'file_count': f'{file_count}', 'bucket_size': f'{bucket_size:.2f}GB'}
     # bucket 명, 폴더 수, 파일 수, 버킷 사이즈 => 소수점 둘째 자리에서 반올림해서 딕셔너리 형식으로 리턴
     return s3_info
